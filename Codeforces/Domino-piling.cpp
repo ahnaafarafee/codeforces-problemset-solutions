70694bac409192aad0f43779bd54e454e61df86e
Ahnaaf Al Rafee <ahnaafarafee@gmail.com>
2021-07-07 13:06:15 +0600

    50/A Domino Piling

---
+++ Codeforces/Domino-piling.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// https://codeforces.com/problemset/problem/50/A
+
+int main()
+{
+
+    int m, n;
+    double res;
+
+    cin >> m >> n;
+
+    res = (m * n) / 2;
+
+    cout << res << endl;
+
+    return 0;
+}
