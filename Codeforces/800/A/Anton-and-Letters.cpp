ac61fa6153daae2199b90a44d36dd432b2e69b45
Ahnaaf Al Rafee <ahnaafarafee@gmail.com>
2021-07-26 13:53:28 +0600

    443/A Anton and Letters

---
+++ Codeforces/800/A/Anton-and-Letters.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// https://codeforces.com/problemset/problem/443/A
+
+int main()
+{
+    int count = 0;
+    string s;
+
+    getline(cin, s);
+
+    sort(s.begin(), s.end());
+
+    for (int i = 0; i < s.size(); i++)
+    {
+        if (s[i] == ' ' || s[i] == '{' || s[i] == '}' || s[i] == ',')
+            continue;
+        else
+        {
+            if (s[i] != s[i + 1])
+                count++;
+        }
+    }
+
+    cout << count << endl;
+
+    return 0;
+}
